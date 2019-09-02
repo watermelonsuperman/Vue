1c68e4c5fc5e50dda92e2da682aeeb7df7b7a90e
watermelonsuperman <15538287230@163.com>
2019-09-02 19:37:56 +0800

    counter

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <input v-model="num"/>
+
+    <counter v-for="n in parseInt(num)" v-bind:key="n" v-bind:counter = "n" v-bind:index = "n" v-on:update="sum += $event"/>
+    sum:{{sum}}
+  </div>
+</template>
+
+<script>
+import counter from './components/counter.vue'
+export default {
+  name: 'app',
+  components: {
+    counter
+  },
+  data(){
+    return{
+      num:0,
+      sum:0
+    }
+  },
+  methods:{
+    
+  }
+
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/counter.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h1></h1>
+    <span>{{index}}、</span>
+
+    <button v-on:click="add">+</button>
+
+    <span>{{number}}</span>
+    <button v-on:click="desc">-</button>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return{
+      number:this.index
+      };
+  },
+  props: {
+    index:Number
+  },
+  created(){
+      this.$emit('update',this.number);
+  },
+  destroyed(){
+      this.$emit('update',-this.number);
+  },
+  methods: {
+      desc(){
+          this.number--;
+          this.$emit('update',-1);
+
+      },
+      add(){
+        this.$emit('update',1);
+        this.number++;
+      }
+
+  }
+
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
